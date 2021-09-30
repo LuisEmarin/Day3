27f6f7681dae3998fed301f77dbd85455ffdfa57
LuisEmarin <90007293+LuisEmarin@users.noreply.github.com>
2021-09-30 10:25:04 -0400

    Add files via upload

---
+++ day-3/jupyterhw.py
@@ -0,0 +1,15 @@
+pi= 3.14
+def circ(int):
+    r = float(input("What is the radius?"))
+    circumference = 2 * pi * r
+    print(circumference)
+
+
+
+def sqfoot(int):
+    length = float(input("What is the length?"))
+    width = float(input("What is the width?"))
+    Area = length * width
+    print(Area)
+
+sqfoot(1)   
